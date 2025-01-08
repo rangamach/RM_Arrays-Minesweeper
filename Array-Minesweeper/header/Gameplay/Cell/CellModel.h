57e35878b378bdfd5a49998526ab75a0cbeb9da3
rangamach <117797904+rangamach@users.noreply.github.com>
2025-01-08 10:53:32 +0530

    added enum classes for cell state and value

--- Array-Minesweeper/header/Gameplay/Cell/CellModel.h
+++ Array-Minesweeper/header/Gameplay/Cell/CellModel.h
@@ -1,11 +1,47 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 namespace Gameplay
 {
 	namespace Cell
 	{
+		enum class CellState
+		{
+			Hidden,
+			Open,
+			Flagged,
+		};
+		enum class CellValue
+		{
+			Empty,
+			One,
+			Two,
+			Three,
+			Four,
+			Five,
+			Six,
+			Seven,
+			Eight,
+			Mine
+		};
 		class CellModel
 		{
+		private:
+			CellState cell_state;
+			CellValue cell_value;
+			sf::Vector2i position;
+
+		public:
+			CellModel();
+			~CellModel();
+
+			CellState GetCellState();
+			void SetCellState(CellState state);
+			CellValue GetCellValue();
+			void SetCellValue(CellValue value);
+			sf::Vector2i GetCellPosition();
+			void SetCellPosition(sf::Vector2i pos);
 		};
 	}
 }
--- Array-Minesweeper/source/Gameplay/Cell/CellModel.cpp
+++ Array-Minesweeper/source/Gameplay/Cell/CellModel.cpp
@@ -2,4 +2,38 @@
 
 using namespace Gameplay::Cell;
 
+CellModel::CellModel()
+{
+}
 
+CellModel::~CellModel() = default;
+
+CellState CellModel::GetCellState()
+{
+	return cell_state;
+}
+
+void CellModel::SetCellState(CellState state)
+{
+	cell_state = state;
+}
+
+CellValue CellModel::GetCellValue()
+{
+	return cell_value;
+}
+
+void CellModel::SetCellValue(CellValue value)
+{
+	cell_value = value;
+}
+
+sf::Vector2i Gameplay::Cell::CellModel::GetCellPosition()
+{
+	return position;
+}
+
+void Gameplay::Cell::CellModel::SetCellPosition(sf::Vector2i pos)
+{
+	position = pos;
+}
